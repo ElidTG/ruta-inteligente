f343d626d477996ae5b44f362ebe3aca8f8f4fcb
ElidTG <torresgonzalezelidailton@gmail.com>
2023-10-17 09:49:42 -0600

    tercer commit
    Se agrega la funcion de buscar. Solo falta que muestre el registro en la interfaz

--- src/app/components/tabla-buscar/tabla-buscar.component.ts
+++ src/app/components/tabla-buscar/tabla-buscar.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
-
+import { catchError } from 'rxjs/operators';
+import { TablaService } from 'src/app/services/tabla/tabla.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-tabla-buscar',
   templateUrl: './tabla-buscar.component.html',
   styleUrls: ['./tabla-buscar.component.css']
 })
 export class TablaBuscarComponent {
+  registroEncontrado: any = null; 
+  registros: any[] = [];
+  Ruta = ''
+  constructor(private tablaService: TablaService) {}
 
+  ngOnInit(): void {
+    // No realices la solicitud de inicio de sesión en el constructor
+    // Puedes hacerlo en un método cuando se dispare un evento, por ejemplo, en el evento de envío de formulario.
+    this.obtenerRegistro();
+    this.Ruta = localStorage.getItem('Ruta') || '';
 }
+  buscarRegistro(rpu: string) {
+    this.tablaService.obtenerRegistroPorId(rpu)
+    .pipe(
+      catchError((error) => {
+        // Manejo de errores
+        console.error('Error al buscar registro:', error);
+        return [];
+      })
+    )
+    .subscribe((data: any) => {
+      if (data.ok) {
+        this.registroEncontrado = data;
+        console.log(this.registroEncontrado);
+      }
+    });
+  }
+  obtenerRegistro(){
+    this.tablaService.obtenerRegistros().subscribe((response: any) => {
+      if (response.ok) {
+        this.registros = response.registros;
+        console.log (this.registros);
+        
+      }
+    });
+}
+}
+function alertaOk (message : string){
+  Swal.fire({
+  text: message,
+  width : 'auto',
+  icon : 'success',
+  iconColor : '#090c9b',
+  background : 'white',
+  padding : '0px 0px 20px 0px',
+  color : 'black',
+  buttonsStyling : false,
+  showConfirmButton : false,
+  timer : 1500
+  });
+  }
--- src/app/services/tabla/tabla.service.ts
+++ src/app/services/tabla/tabla.service.ts
@@ -8,11 +8,26 @@ export class TablaService {
   private api = 'http://localhost:3000'; 
 
   constructor(private http: HttpClient) { }
+  //Agregar un nuevo registro
   agregarRegistro(registro: any) {
     return this.http.post(`${this.api}/patabla`, registro);
   }
-
+ // Obtener todos los registros
   obtenerRegistros() {
     return this.http.get(`${this.api}/gettabla`);
+}
+ // Obtener un registro por ID
+ obtenerRegistroPorId(rpu: string) {
+  return this.http.get(`${this.api}/gettabla/${rpu}`);
+}
+
+// Actualizar un registro por ID
+actualizarRegistro(id: string, registro: any) {
+  return this.http.put(`${this.api}/actualizartabla/${id}`, registro);
+}
+
+// Eliminar un registro por ID
+eliminarRegistro(id: string) {
+  return this.http.delete(`${this.api}/eliminartabla/${id}`);
 }
 }
