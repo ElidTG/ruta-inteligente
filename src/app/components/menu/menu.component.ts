83d5c3bdb278145d7824863a4ce5cb38d6d5c263
ElidTG <torresgonzalezelidailton@gmail.com>
2023-10-26 20:46:58 -0600

    menu deployed

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,16 +1,12 @@
 // menu.component.ts
 
-import { AuthService } from '../auth.service'; // Importa el servicio
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-  constructor(private authService: AuthService) {}
 
-  estaAutenticado(): boolean {
-    return this.authService.estaAutenticado;
-  }
 }
