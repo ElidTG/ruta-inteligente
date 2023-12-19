1c37ef6239d967d62efe1518e06c25666091e2ed
ElidTG <torresgonzalezelidailton@gmail.com>
2023-12-18 22:24:37 -0600

    quagga 5.2

--- src/app/components/camara-registro/camara-registro.component.ts
+++ src/app/components/camara-registro/camara-registro.component.ts
@@ -26,17 +26,15 @@ export class CamaraRegistroComponent {
     inputStream: {
       name: "Live",
       type: "LiveStream",
-      target: document.querySelector('#results'),
+      target: document.querySelector('#barcode-scanner'),
     },
     decoder: {
-      readers: ['code_128_reader', 'ean_reader'],
+      readers: ['code_128_reader', 'ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader'],
     },
-  }, (err: any) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    // Inicializar Quagga después de configurarlo
+  }, function(err: any) {
+    if (err) { console.log(err); return }
+        // Inicializar Quagga después de configurarlo
+    Quagga.initialized = true;
     Quagga.start();
   });
   }
